a4d79a11b2e97bab854550dd8ae64cc52e54daf7
孙树超 <sunshuchao@xmfunny.com>
2025-03-17 10:30:19 +0800

    <add> 定义断言，并且只在 debug 下开启断言

--- Hazel/src/Hazel/Core/Core.h
+++ Hazel/src/Hazel/Core/Core.h
@@ -5,6 +5,9 @@ namespace Hazel {
 	void ShutdownCore();
 }
 
+#ifdef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS
+#endif
 
 #ifdef HZ_PLATFORM_WINDOWS
 	#ifdef HAZEL_BUILD_DLL
@@ -14,4 +17,12 @@ namespace Hazel {
 	#endif
 #else
 	#error Hazel only supports Windows!
+#endif
+
+#ifdef HZ_ENABLE_ASSERTS
+	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define HZ_ASSERT(x, ...)
+	#define HZ_CORE_ASSERT(x, ...)
 #endif
